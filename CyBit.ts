4db1b4108fd41285b4f4b837d62a12a1283bac1e
PrinceBot-Ratthanin <50706633+PrinceBot-Ratthanin@users.noreply.github.com>
2023-06-27 22:53:57 +0700

    Update CyBit.ts
    Signed-off-by: PrinceBot-Ratthanin <50706633+PrinceBot-Ratthanin@users.noreply.github.com>

--- CyBit.ts
+++ CyBit.ts
@@ -167,7 +167,7 @@ namespace CyBit {
     //% block="digital read |%selectpins|"
     //% weight=99
     export function digitalRead(selectpins: DigitalPin): number {
-        led.enable(false)
+        led.enable(false);
         return pins.digitalReadPin(selectpins);
         
     }
@@ -180,7 +180,7 @@ namespace CyBit {
     //% block="digital write %selectpins | status %Pinstatus "
     //% weight=98
     export function digitalWrite(selectpins: DigitalPin, Status: number): void {
-        led.enable(false)
+        led.enable(false);
         pins.digitalWritePin(selectpins,Status);
         // body...
     }
