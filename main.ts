e3127b500b05f7835a3c4d7a022d44f0bd9b7db7
PrinceBot-Ratthanin <50706633+PrinceBot-Ratthanin@users.noreply.github.com>
2023-07-14 22:35:20 +0700

    Update pxt.json, main.blocks, main.ts, CyBit.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,41 @@
+CyBit.Motor_Stop(motorCH.M1234)
+led.enable(false)
+OLED12864_I2C.init(60)
 basic.forever(function () {
-	
+    CyBit.MotorRun(motorCH.M1234, motorDIR.Forward, 50)
+    CyBit.ServoRun(CyBit.servoPort.S1, 90)
+    CyBit.digitalWrite(DigitalPin.P10, 0)
+    OLED12864_I2C.clear()
+    OLED12864_I2C.showNumber(
+    0,
+    0,
+    CyBit.analogRead(CyBit.analogPort.P1),
+    1
+    )
+    OLED12864_I2C.showNumber(
+    7,
+    0,
+    CyBit.analogRead(CyBit.analogPort.P2),
+    1
+    )
+    CyBit.MotorRun(motorCH.M1234, motorDIR.Forward, 50)
+    basic.pause(100)
+    CyBit.MotorRun(motorCH.M1234, motorDIR.Reverse, 50)
+    CyBit.digitalWrite(DigitalPin.P10, 1)
+    CyBit.ServoRun(CyBit.servoPort.S1, 100)
+    OLED12864_I2C.clear()
+    OLED12864_I2C.showNumber(
+    0,
+    0,
+    CyBit.analogRead(CyBit.analogPort.P1),
+    1
+    )
+    OLED12864_I2C.showNumber(
+    7,
+    0,
+    CyBit.analogRead(CyBit.analogPort.P2),
+    1
+    )
+    basic.pause(100)
+    CyBit.MotorRun(motorCH.M1234, motorDIR.Reverse, 50)
 })
